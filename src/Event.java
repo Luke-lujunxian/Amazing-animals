54405ff40dc2ef299abf641b889b503d954e6c20
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2017-11-08 08:17:24 +0800

    修改基因交换方法
    由随机分段改为每个基因随机。随机仅使用了Math.random，可能存在轻微概率不平均

--- src/Event.java
+++ src/Event.java
@@ -1,9 +1,21 @@
 public class Event {
     public Individual reproduc(Individual a,Individual b){
-        int swichPoint = (int)Math.random()*20;
+        //int swichPoint = (int)(Math.random()*20);
+
         Individual newSon = new Individual();
-        System.arraycopy(a.gean,0,newSon.gean,0,swichPoint);
-        System.arraycopy(b.gean,swichPoint,newSon.gean,swichPoint,b.gean.length - swichPoint-1);
+
+        for  (int i=0;i<newSon.gean.length;i++){
+           double choose = Math.random();
+            if(choose>=0.5)
+                newSon.gean[i]=a.gean[i];
+            else
+                newSon.gean[i]=b.gean[i];
+        }
+        /*对每个基因随机选择组合*/
+
+        //废弃代码
+        //System.arraycopy(a.gean,0,newSon.gean,0,swichPoint);
+        //System.arraycopy(b.gean,swichPoint,newSon.gean,swichPoint,b.gean.length - swichPoint-1);
 
         return newSon;
     }
