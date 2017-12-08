8b9dde7f8e1e797681026545419e2b94066f1ebd
HONG ZIXIANG <291043595@qq.com>
2017-12-08 17:41:54 +0800

    dlt12.8更新
    实现了decode方法
    定义了fitness方法

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
+     public static void fitness(Individual a) {
+        int i=0;
+        int j[]=decode(a.gene);
+        //compute fitness
+     }
+     public static int[] decode(boolean[][] a) {
+         int[] h=new int[a.length];
+         for(int i=0;i<a.length;i++){
+             int j=0;
+             for(int k=0;k<a[i].length;k++) if(a[i][k]) if(k>0) j+=2*k; else j+=1;
+             switch (i) {
+                 case 0:
+                 case 1:
+                    h[i]=j*10;
+                    continue;
+                 default:
+                     h[i]=j+1;
+             }
+         }
+         return h;
+    }
     public static void main(String args[]){
-       System.out.println("神奇动物都死了！");
        Individual setOfIndividual[] = new Individual[100];
        Environment env1 = new Environment();
     }
