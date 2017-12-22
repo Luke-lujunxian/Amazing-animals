308d870241395a25e68ae2e850bf20d55205d419
Hudayday <32944717+HudayMuhammad@users.noreply.github.com>
2017-12-22 08:25:57 +0800

    12.22 Mise à jour de l'interface graphique

--- src/Fenestra.java
+++ src/Fenestra.java
@@ -2,15 +2,20 @@ import java.awt.*;
 import javax.swing.*;
 public class Fenestra {
     private static JFrame bufo;
-    private static Button b1;
-    private static Button b2;
+    private static JButton b1;
+    private static JButton b2;
+    private static JLabel dalao;
     
     public static void Fenestra(String[] args) {
     bufo=new JFrame("Bufonem Emittunt Aeternum");
     bufo.setSize(400,400);
     bufo.setLayout(new FlowLayout());
-    b1 = new Button("Initium");  
-    b2 = new Button("Finis");            
+    b1 = new JButton("Initium");  
+    b2 = new JButton("Finis"); 
+    dalao = new JLabel(" ");
+    dalao.setText("真大佬");
+    dalao.setLocation(300, 300);
+    dalao.setSize(40, 10);
     bufo.add(b1);        
     bufo.add(b2);
     bufo.setBackground(Color.green);
