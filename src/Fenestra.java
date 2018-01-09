cd21ad719023e94477f981ff5340ac57420cf57c
Hudayday <32944717+HudayMuhammad@users.noreply.github.com>
2018-01-09 08:27:09 +0800

    1.9窗口更新
    更新了窗口，可以读取和编辑环境变量。

--- src/Fenestra.java
+++ src/Fenestra.java
@@ -1,26 +1,66 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 public class Fenestra {
-    private static JFrame bufo;
+    private JFrame bufo;
     private static JButton b1;
     private static JButton b2;
+    private static JButton b3;
+    private static JButton b4;
+    private static JButton b5;
     private static JLabel dalao;
-    
-    public static void Fenestra(String[] args) {
+    public Fenestra() {
+        Environment io = new Environment();
+    b1 = new JButton("温度"+io.temperature);  
+    b2 = new JButton("湿度"+io.humidity);
+    b3 = new JButton("地形"+io.terrain);
+    b4 = new JButton("氧气"+io.oxygenLevel);
+    b5 = new JButton("食物"+io.food);
     bufo=new JFrame("Bufonem Emittunt Aeternum");
     bufo.setSize(400,400);
     bufo.setLayout(new FlowLayout());
-    b1 = new JButton("Initium");  
-    b2 = new JButton("Finis"); 
     dalao = new JLabel(" ");
     dalao.setText("真大佬");
     dalao.setLocation(300, 300);
     dalao.setSize(40, 10);
     bufo.add(b1);        
     bufo.add(b2);
+    bufo.add(b3);
+    bufo.add(b4);
+    bufo.add(b5);
     bufo.setBackground(Color.green);
     bufo.setVisible(true);
     bufo.setDefaultCloseOperation(bufo.EXIT_ON_CLOSE);
+    b1.addMouseListener(new MouseAdapter() {
+        private JFrame bot1;  
+        private JLabel bot11;
+        private JTextField text1;
+        public void mouseClicked(MouseEvent e) {
+            bufo=new JFrame("输入温度");
+         bot1.setSize(200,200);
+    bot1.setLayout(new FlowLayout());
+    bot11 = new JLabel(" ");
+    bot11.setText("输入温度"); 
+    bufo.setBackground(Color.green);
+    bufo.setVisible(true);
+    bufo.setDefaultCloseOperation(bufo.EXIT_ON_CLOSE);
+    text1.setSize(100,10);
+    text1.addKeyListener(new KeyListener() {
+    public void keyTyped(KeyEvent e) {};
+
+    public void keyPressed(KeyEvent e) {
+    if(e.getKeyCode()==KeyEvent.VK_ENTER){String a = text1.getText();
+    io.setTemperature(Integer.valueOf(a));
+    bot1.dispose();}
+        };
+    
+    public void keyReleased(KeyEvent e) {};
+    });
+        }
+    });
+    }
+    public static void Fenestra(String[] args) {
+    
     
     }
     
