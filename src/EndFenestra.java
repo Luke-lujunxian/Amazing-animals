396b1c4585130e28f1e61ef1916b83563fca719e
Hudayday <32944717+HudayMuhammad@users.noreply.github.com>
2018-01-12 06:49:12 +0800

    Update EndFenestra.java

--- src/EndFenestra.java
+++ src/EndFenestra.java
@@ -4,12 +4,31 @@ import javax.swing.*;
 public class EndFenestra {
     private JFrame bufo;  
     private static JButton b6;
-    public EndFenestra() {
+    private static JButton b5;
+    public EndFenestra(String n) {
     b6 = new JButton("退出");
-    bufo=new JFrame("Bufonem Emittunt Aeternum");
-    
+    bufo=new JFrame(n);
     bufo.setSize(400,400);
     bufo.setLayout(new FlowLayout());
+    bufo.add(b6);
+    b6.setSize(150,50);
+    bufo.setBackground(Color.green);
+    bufo.setVisible(true);
+    bufo.setDefaultCloseOperation(bufo.EXIT_ON_CLOSE);
+    b6.addMouseListener(new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            System.out.print("神奇动物已经完蛋了");
+            bufo.dispose();
+        }
+    });
+    }
+    public EndFenestra(String n,String k) {
+    b6 = new JButton("退出");
+    b5 = new JButton(k);
+    bufo=new JFrame(n);
+    bufo.setSize(400,400);
+    bufo.setLayout(new FlowLayout());
+    bufo.add(b5);
     bufo.add(b6);
     b6.setSize(150,50);
     bufo.setBackground(Color.green);
